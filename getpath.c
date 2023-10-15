ceee660f63309ec6a5e3b0ab61074b03ae9d309f
EsraaGD <isra.hatim2@gmail.com>
2023-10-14 17:19:14 -0700

    Wai

--- getpath.c
+++ getpath.c
@@ -9,36 +9,30 @@ char *srn_getpath(char *vampcmd)
 {
 	char *path_env, *full_vcmd, *direc;
 	int v;
-	struct stat rv;
 
-	/* if vampcmd is already path */
 	for (v = 0; vampcmd[v]; v++)
 	{
 		if (vampcmd[v] == '/')
 		{
-			if (stat(vampcmd, &rv) == 0) /* if path exist */
+			if (access(vampcmd, X_OK) == 0) /* if path exist */
 				return (srn_duplicate(vampcmd));
-
 			return (NULL);
 		}
 	}
-	/* if user unset path (can not get dir) */
 	path_env = srn_getenv("PATH");
 	if (!path_env)
 		return (NULL);
 
-	/* try handle path */
 	direc = strtok(path_env, ":");
 	while (direc)
 	{
-		/* size = len(direc) + len(vampcmd) + 2 ('/' and '\0') */
 		full_vcmd = malloc(srn_lenght(direc) + strlen(vampcmd) + 2);
 		if (full_vcmd)
 		{
 			srn_copy(full_vcmd, direc);
 			srn_cat(full_vcmd, "/");
 			srn_cat(full_vcmd, vampcmd);
-			if (stat(full_vcmd, &rv) == 0)
+			if (access(full_vcmd, X_OK) == 0)
 			{
 				free(path_env);
 				return (full_vcmd);
