fe276fed108b11f797f833bc11f2a953a2414633
Noon47714 <noon47714@gmail.com>
2023-10-15 09:49:26 -0700

    task

--- getpath.c
+++ getpath.c
@@ -1,49 +1,48 @@
 #include "srn.h"
 
 /**
- * srn_getpath - the path of usre
- * @vampcmd: the command
- * Return: NULL.
+ * srn_getpath - the user path
+ * @vampcmd: command
+ * Return: NULL
  */
 char *srn_getpath(char *vampcmd)
 {
 	char *path_env, *full_vcmd, *direc;
-	int v;
-	struct stat rv;
+	int i;
+	struct stat st;
 
-	/* if vampcmd is already path */
-	for (v = 0; vampcmd[v]; v++)
+	for (i = 0; vampcmd[i]; i++)
 	{
-		if (vampcmd[v] == '/')
+		if (vampcmd[i] == '/')
 		{
-			if (stat(vampcmd, &rv) == 0) /* if path exist */
+			if (stat(vampcmd, &st) == 0)
 				return (srn_duplicate(vampcmd));
 
 			return (NULL);
 		}
 	}
-	/* if user unset path (can not get dir) */
+
 	path_env = srn_getenv("PATH");
 	if (!path_env)
 		return (NULL);
 
-	/* try handle path */
 	direc = strtok(path_env, ":");
 	while (direc)
 	{
-		/* size = len(direc) + len(vampcmd) + 2 ('/' and '\0') */
-		full_vcmd = malloc(srn_lenght(direc) + strlen(vampcmd) + 2);
+		full_vcmd = malloc(srn_lenght(direc) + srn_lenght(vampcmd) + 2);
 		if (full_vcmd)
 		{
 			srn_copy(full_vcmd, direc);
 			srn_cat(full_vcmd, "/");
 			srn_cat(full_vcmd, vampcmd);
-			if (stat(full_vcmd, &rv) == 0)
+
+			if (stat(full_vcmd, &st) == 0)
 			{
 				free(path_env);
 				return (full_vcmd);
 			}
 			free(full_vcmd), full_vcmd = NULL;
+
 			direc = strtok(NULL, ":");
 		}
 	}
